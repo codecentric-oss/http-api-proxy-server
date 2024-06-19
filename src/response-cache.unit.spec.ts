b4664e38b5b1cbe1cb008aa8ad7f400e5b8625e1
Richard Siegel <richard@siegel.work>
2024-06-19 10:05:41 +0200

    test: make cache unit tests os agnostic

--- src/response-cache.unit.spec.ts
+++ src/response-cache.unit.spec.ts
@@ -1,5 +1,6 @@
 import { Request, createRequestId } from "./proxy-server";
 import { ResponseCacheConnector } from "./response-cache";
+import path from "path";
 
 const createRequest = (overwrites: Partial<Request>): Request => ({
   requestId: createRequestId({
@@ -20,13 +21,13 @@ describe("ResponseCacheConnector", () => {
 
   test("filePathForHash will build valid path", () => {
     expect(cache.filePathForRequestId("hash")).toEqual(
-      "test/responses/testName/hash.json"
+      path.join("test", "responses", "testName", "hash.json")
     );
   });
 
   test("filePathForRequest will build valid path", () => {
     expect(cache.filePathForRequest(req1927740808)).toEqual(
-      "test/responses/testName/responseFor1927740808.json"
+      path.join("test", "responses", "testName", "responseFor1927740808.json")
     );
   });
 });
